1d498b07310a2b226f8465d56193ba6a6dab79db
Pooja Sastry <pooja.sastry@thoughtworks.com>
2026-07-07 13:41:53 +0530

    Pooja | Hive-121641| [Security Fix] [Bahmni Core] Security Fix (#42)
    * BSL-14 | Fix. SQL Exfiltration vulnerability
    
    * BSL-13 | Fix. Path traversal vulnerability on AdminImport
    
    - Files are created with random filenames.
    - Introduced and added privilege check for file upload.
    - Added test for AdminImportController. Minor code improvement.
    
    * BSL-14|Re added the deleted test case (#334)
    
    * BSL-13,BSL-14 | Fix. Update Maven Central publish version to 0.11.0 (#335)
    
    * Pooja | Hive-121641 | Add an additional empty list check
    
    ---------
    
    Co-authored-by: ravinderkabli <ravinder.kabli@thoughtworks.com>
    Co-authored-by: angshuman sarkar <angshus@thoughtworks.com>
    Co-authored-by: MOHANKUMAR T <31698165+mohan-13@users.noreply.github.com>

--- bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/security/PrivilegeConstants.java
+++ bahmnicore-api/src/main/java/org/bahmni/module/bahmnicore/security/PrivilegeConstants.java
@@ -2,4 +2,5 @@ package org.bahmni.module.bahmnicore.security;
 
 public class PrivilegeConstants {
     public static final String DELETE_PATIENT_DOCUMENT_PRIVILEGE = "Delete Patient Document";
+    public static final String IMPORT_CSV_FILE_PRIVILEGE = "Import CSV Files";
 }
